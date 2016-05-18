e90ceb32278062c4f76f417b7d6b9ed8526b39e7
robintully <mr.robintully@gmail.com>
2016-05-18 15:47:55 -0400

    added random GIF generator

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,4 +1,11 @@
 class Gif < ActiveRecord::Base
+  attr_reader :url
+
+  def self.random
+    newgif = Gif.new
+    newgif.url=JSON.load(open("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"))['data']['url']
+    newgif.save
+  end
 
 
 
